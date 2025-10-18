0df420d78272a18baa0ff69340aa1451b64697d9
amf4c@umsystem.edu <amf4c@umsl.edu>
2025-10-18 15:37:35 -0500

    Final modifications to custom Transformer

--- customTransformer.py
+++ customTransformer.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 import math
-import copy
+
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, n_heads, dropout: float):
